de18368bb930dddbbfbf7eebce3ce980c3936182
spiskorski <stas.piskorski@gmail.com>
2025-12-02 13:27:42 +0100

    Added reading from config file in calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -1,4 +1,6 @@
 import sys
+import argparse
+from email.policy import default
 
 
 def add(x, y):
@@ -7,7 +9,34 @@ def add(x, y):
 def product(x, y):
     return x * y
 
-def main():
+def divide(x, y):
+    if y == 0:
+        raise ValueError('We cannot divide by y = 0.')
+    return x/ y
+
+def read_configuration(path: str):
+    with open(path, 'r') as f:
+        line1 = f.read()
+
+    operation, x, y = [element.strip() for element in line1.split(',')]
+    return {'operation': operation, 'x':x, 'y':y}
+
+def run_from_file(path: str):
+    conf = read_configuration(path)
+    x = int(conf['x'])
+    y = int(conf['y'])
+    operation = conf['operation']
+    if operation == 'add':
+        result = add(x, y)
+    elif operation == 'mul':
+        result = product(x, y)
+    else:
+        print("Incorect choice.")
+        return -1
+    print(f'Your result {result}')
+    return 0
+
+def terminal_run():
     print("Select operation:")
     print("1. Addition")
     print("2. Multiplication")
@@ -26,6 +55,22 @@ def main():
 
     print(f"Your result: {result}")
     return 0
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Simple calculator.')
+
+    parser.add_argument(
+        '--file',
+        default = '',
+        help = 'Configuration file'
+    )
+
+    args = parser.parse_args()
+    if args.file == '':
+        return terminal_run()
+
+    return run_from_file(args)
 
 if __name__ == "__main__":
     sys.exit(main())
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,61 @@
 import sys
-from calculator import add
+import unittest
+from calculator import add, product, divide
+from unittest import TestCase, main
 
-print(sys.path)
-print(add(3, 4))
+# assertEqual
+
+class TestAddition(TestCase):
+    def test_add_two_positive_numbers(self):
+        cases = [((2, 3), 5),
+                 ((1, 7), 8),
+                 ((3, 6), 9),
+                 ((1, 1), 2)]
+
+        for args, result in cases:
+            with self.subTest(x=args):
+                self.assertEqual(add(*args), result)
+
+    def test_two_negative_numbers(self):
+        self.assertEqual(add(-4, -5), -9)
+
+class TestProduct(TestCase):
+    def test_multiply_two_positive_numbers(self):
+        self.assertEqual(product(3, 4),12)
+        self.assertGreater(product(2, 3),0)
+
+    def test_multiply_two_negative_numbers(self):
+        self.assertEqual(product(-3, -4), 12)
+        self.assertGreater(product(-2, -3), 0)
+
+    def test_multiply_different_signs(self):
+        self.assertEqual(product(-3, 5), -15)
+        self.assertLess(product(-3, 5), 0)
+
+class TestDivision(TestCase):
+    def test_divide_positive_numbers(self):
+        self.assertEqual(divide(1, 4), 0.25)
+
+    def test_division_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(4, 0)
+
+
+# @unittest.skip
+# @unittest.skipIf
+# @unittest.skipUnless
+
+class SkipTestExample(unittest.TestCase):
+    @unittest.skip('this test is not important')
+    def test_unimportant(self):
+        self.fail('this test should be skipped')
+
+    @unittest.skipIf(sys.version_info < (3, 12), 'Requires python >= 3.12.')
+    def test_using_calendar_constants(self):
+        import calendar
+
+        self.assertEqual(calendar.Month(10), calendar.OCTOBER)
+
+
+if __name__ == '__main__':
+    main(verbosity=2)
